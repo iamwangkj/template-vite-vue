94846afe33a471e0ae9ef4b17ca99b5d9f8f8f1c
wangkj <ne.wkj@qq.com>
2021-03-03 23:38:14 +0800

    add head

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <button @click="generate">生成</button>
+
+    <div ref="img0">
+      <Head />
+    </div>
     <div ref="img1">
       <Info />
     </div>
@@ -10,6 +14,7 @@
     <!-- 以下是预览图片 -->
     <div class="preview">
       <h1>以下是预览图片</h1>
+      <a :href="imgUrl0" download="0" class="part-img-wrapper"><img :src="imgUrl0" /></a>
       <a :href="imgUrl1" download="1" class="part-img-wrapper"><img :src="imgUrl1" /></a>
       <a :href="imgUrl2" download="2" class="part-img-wrapper"><img :src="imgUrl2" /></a>
     </div>
@@ -19,17 +24,20 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import html2canvas from 'html2canvas'
+import Head from './components/Head.vue'
 import Info from './components/Info.vue'
 import Lightspot from './components/Lightspot.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    Head,
     Info,
     Lightspot
   },
   data () {
     return {
+      imgUrl0: '',
       imgUrl1: '',
       imgUrl2: ''
     }
@@ -38,6 +46,11 @@ export default defineComponent({
   },
   methods: {
     generate () {
+      html2canvas(this.$refs.img0).then((canvas) => {
+        const base64Str = canvas.toDataURL('image/jpeg')
+        this.imgUrl0 = base64Str
+      })
+
       html2canvas(this.$refs.img1).then((canvas) => {
         const base64Str = canvas.toDataURL('image/jpeg')
         this.imgUrl1 = base64Str
@@ -61,14 +74,16 @@ export default defineComponent({
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   color: #2c3e50;
+  font-size: 24px;
   /* background: linear-gradient(to right, pink, white); */
 }
 
 .section {
+  box-sizing: border-box;
   padding: 0 10px 20px;
 }
 .section-title {
-  font-size: 20px;
+  /* font-size: 20px; */
 }
 
 .preview {
---
+++ src/components/Head.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="section head">
+    <div class="title-cn">花芯</div>
+    <div class="sku"></div>
+    <div class="circle1">灵动</div>
+    <div class="circle2">闪耀</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {},
+  setup (props) {},
+  data () {
+    return {
+
+    }
+  }
+})
+</script>
+
+<style scoped>
+.head {
+  width: 750px;
+  height: 905px;
+  background: url('../assets/bg-head.jpg') center/cover no-repeat;
+  position: relative;
+}
+
+.title-cn {
+  /* background: linear-gradient(to right, #733c2f, #e78162); */
+  color: #c97861;
+  font-size: 42px;
+  text-align: center;
+  position: relative;
+  top: 190px;
+}
+
+.sku {
+  width: 626px;
+  height: 626px;
+  background: url('../assets/sku1.png') center/contain no-repeat;
+  margin: 0 auto;
+  padding-top: 100px;
+}
+
+.circle1 {
+  width: 168px;
+  height: 155px;
+  background: url('../assets/icon-circle.png') center/cover no-repeat;
+  line-height: 155px;
+  color: #e78162;
+  text-align: center;
+  font-size: 40px;
+  position: absolute;
+  left: 10px;
+  top: 438px;
+}
+
+.circle2 {
+  width: 116px;
+  height: 116px;
+  line-height: 116px;
+  background: url('../assets/icon-circle.png') center/cover no-repeat;
+  color: #e78162;
+  text-align: center;
+  font-size: 36px;
+  position: absolute;
+  left: 540px;
+  top: 580px;
+}
+</style>
